df05099d2ee9530274580d500dfdb96d336a0dda
Admir Dizdarevic <cpt@pop-os.localdomain>
2023-03-12 23:23:01 +0100

    First commit

---
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.einzelbeispiel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+    }
+}
